721ce51a9f18564fcc9db3257a3f24200157d1e1
Robert Schuh <67490494+raschuh@users.noreply.github.com>
2024-01-02 10:41:21 -0700

    Add a header to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <></>
+    <div>
+      <Header />
+      <main className='app-main-container'>
+
+      </main>
+    </div>
   );
 }
 
