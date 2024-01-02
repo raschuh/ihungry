8871d12f39a1ec5668bb62a81b9da909f8e18508
Robert Schuh <67490494+raschuh@users.noreply.github.com>
2024-01-02 13:19:36 -0700

    Create Search component

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import './Search.css';
+
+function Search() {
+  return (
+    <div>
+      <input type='text' placeholder='Search by ingredients...'/>
+    </div>
+  );
+}
+
+export default Search;
