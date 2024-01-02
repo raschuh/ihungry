249db0e1a3453fe35f4a17da3599df577195f369
Robert Schuh <67490494+raschuh@users.noreply.github.com>
2024-01-02 10:41:00 -0700

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className='header-banner'>
+      <div className='header-body-container'>
+        <h1 className='header-title'>i<span className='header-span-title'>hungry</span></h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
