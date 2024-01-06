bb80c1b6989a0994af997273d9cce80f811db1fb
Robert Schuh <67490494+raschuh@users.noreply.github.com>
2024-01-05 19:59:20 -0700

    Publish the app build 6cc569c

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 import './App.css';
-import Footer from './components/Footer';
 
 function App() {
   return (
