eb0dcd9faa4f41d53395f9429a902dae667db7d7
Robert Schuh <67490494+raschuh@users.noreply.github.com>
2024-01-11 14:42:25 -0700

    Fix Issue #4 -- download smaller preview image instead

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Card({ to, item }) {
+  // Try load a smaller preview version first
+  const [imgSrc, setImgSrc] = useState(item.image + '/preview');
+
+  // If cannot load preview image then load full sized image
+  function cannotLoadPreview() {
+    setImgSrc(item.image);
+  }
+
   return (
     <Link to={to}>
       <div className='flex flex-col border border-slate-400 shadow-lg'>
-        <img src={item.image} alt={item.str} />
+        <img src={item.image} alt={item.str} onError={cannotLoadPreview} />
         <div className='bg-dark text-light text-center'>
           <h3>{item.str}</h3>
         </div>
