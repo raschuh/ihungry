d75a622548c8fb73e20645e242476ab6e6092970
Robert Schuh <67490494+raschuh@users.noreply.github.com>
2024-01-14 14:00:20 -0700

    Add a route to the country meals page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Search from './components/Search';
 import Category from './components/Category';
 import Ingredient from './components/Ingredient';
+import Country from './components/Country';
 import Meal from './components/Meal';
 
 function App() {
@@ -13,11 +14,12 @@ function App() {
       <Header />
       <main className='max-w-lg mx-auto px-3'>
       <Routes>
-        <Route path='/' element={<Search />} />
+        <Route exact path='/' element={<Search />} />
         <Route path='/category/:name' element={<Category />} />
         <Route path='/ingredient/:name' element={<Ingredient />} />
+        <Route path='/area/:name' element={<Country />} />
         <Route path='/meal/:id' element={<Meal />} />
-        <Route path='*' element={<Error />} />
+        <Route element={<Error />} />
       </Routes>
       </main>
       <Footer />
